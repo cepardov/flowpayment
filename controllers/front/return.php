a016d83333b3a5b8e8d9f09c26e3530482dadccc
Cristian Pardo <cristian.pardo@externos.bci.cl>
2021-11-24 00:22:39 -0300

    23112021

--- controllers/front/return.php
+++ controllers/front/return.php
@@ -19,12 +19,17 @@ class FlowPaymentWPReturnModuleFrontController extends ModuleFrontController
 {
     public $ssl = true;
 
+    public function __construct(){
+        parent::__construct();
+    }
+
     public function initContent()
     {
         $this->display_column_left = false;
         $this->display_column_right = false;
 
         parent::initContent();
+        $this->setTemplate('module:flowpaymentwp/views/templates/front/confirmation.tpl');
         $this->returnPayment();
     }
 
